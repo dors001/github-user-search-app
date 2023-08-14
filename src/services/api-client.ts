b5de9c293c656aefd50582c1e06ee97b24069cd2
dors001 <dorarishani@gmail.com>
2023-08-15 02:45:37 +0300

    environmental variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-//TODO: fix global variables in .env
-const apiKey = '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'
-
 export default axios.create({
     baseURL: `https://api.github.com/users/`,
     params: {
-        key: apiKey
+        key: import.meta.env.REACT_APP_GITHUB_API_KEY
     }
 })
